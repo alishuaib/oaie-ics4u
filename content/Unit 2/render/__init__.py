4e3382ea1a790e0083fddda0344177e04917e189
Ali Shuaib <alishuaib2001as@gmail.com>
2024-04-15 11:23:47 -0400

    Commit for oaie-ics4u

--- content/Unit 2/render/__init__.py
+++ content/Unit 2/render/__init__.py
@@ -1,10 +1,1 @@
 from .demo import show_demo
-
-try:
-    import micropip
-    async def installation():
-        await micropip.install('ipywidgets')
-        await micropip.install('pillow')
-    installation()
-except ImportError:
-    print("No micropip (Pyodide env) found, skipping installation of dependencies")
